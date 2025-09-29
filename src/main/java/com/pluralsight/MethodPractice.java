87129e87d6d4023aa25c244ed55095a7c6c9fb48
YairVera <yairvera44@gmail.com>
2025-09-29 12:39:50 -0500

    Method Pracitce

---
+++ src/main/java/com/pluralsight/MethodPractice.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+public class MethodPractice {
+
+    public static void main(String[] args) {
+        fish();
+        dance();
+        sayHi();
+    }
+
+    public static void sayHi() {
+        System.out.println("Hi");
+
+    }
+
+    public static void dance(){
+        System.out.println("Woahhhhhohhhh");
+        System.out.println("Woahhhhhohhhh");
+        System.out.println("Stop!");
+        System.out.println("Hammer time!");
+    }
+
+    public static void fish(){
+        System.out.println("Get out fishing rod");
+        System.out.println("Bait the hook");
+        System.out.println("Cast");
+        System.out.println("Wait....forever");
+    }
+}
